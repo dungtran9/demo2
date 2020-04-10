b2bfdf15d6c0f779d99735d3e15ef45ffda32655
dungtran9 <63217229+dungtran9@users.noreply.github.com>
2020-04-10 10:13:42 +0700

    o

---
+++ area.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth=prompt("nhap gia tri width");
+inputHeight=prompt("nhap gia tri Height");
+let w=parseInt(inputWidth);
+let h=parseInt(inputHeight);
+let area=w*h;
+document.write("The area is: " + area);
